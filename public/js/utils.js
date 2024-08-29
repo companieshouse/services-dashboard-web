1df8e4b5fb57a1c2648abde80c0c9d4e7d96617e
stefano basile <sbasile@companieshouse.gov.uk>
2024-08-29 11:44:13 +0100

    tidy up

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,5 +1,7 @@
-
- document.addEventListener('DOMContentLoaded', () => {
+//-----------------------------------
+// attach sort handlers to table headers
+//-----------------------------------
+document.addEventListener('DOMContentLoaded', () => {
    const table = document.getElementById('table-services');
    const headers = table.querySelectorAll('th');
    let sortDirection = 1; // 1 for ascending, -1 for descending
@@ -32,8 +34,22 @@
    });
 });
 
+//-----------------------------------
+// function to get all the name & version checkboxes
+//-----------------------------------
+function getAllCheckboxes() {
+   // Select only the checkboxes with the specified classes
+   const nameCheckboxes = document.querySelectorAll('.name-checkbox');
+   const versionCheckboxes = document.querySelectorAll('.version-checkbox');
 
+   // Combine into 1 array
+   return [...nameCheckboxes, ...versionCheckboxes];
+}
+
+
+//-----------------------------------
 // Add handler to check boxes
+//-----------------------------------
 document.addEventListener('DOMContentLoaded', () => {
    const masterCheckbox = document.getElementById('masterCheckbox');
    const nameCheckboxes = document.querySelectorAll('.name-checkbox');
@@ -54,7 +70,9 @@ document.addEventListener('DOMContentLoaded', () => {
          }
       });
    }
-
+   //-------------
+   //  attach to NAME - checkboxes
+   //-------------
    masterCheckbox.addEventListener('change', () => {
       const isChecked = this.checked;
       nameCheckboxes.forEach(checkbox => {
@@ -66,6 +84,9 @@ document.addEventListener('DOMContentLoaded', () => {
       filterTable();
    });
 
+   //-------------
+   //  attach to VERSION - checkboxes
+   //-------------
    nameCheckboxes.forEach(checkbox => {
       checkbox.addEventListener('change', () => {
          const name = this.id;
@@ -82,125 +103,38 @@ document.addEventListener('DOMContentLoaded', () => {
    versionCheckboxes.forEach(checkbox => {
       checkbox.addEventListener('change', filterTable);
    });
-
-   sourceState();
+   // sourceState();
    filterTable(); // Initial filter on page load
-
-  document.getElementById('button-share').addEventListener('click', async () => {
-      try {
-         const base64State = generateState();
-         const response = await fetch(`/dashboard?savestate=${encodeURIComponent(base64State)}`);
-         if (response.ok) {
-            const linkId = await response.text();
-            const currentUrl = window.location.href;
-            // Get the base URL without query parameters
-            const baseUrl = window.location.origin + window.location.pathname;
-            const shareUrl = `${baseUrl}?linkid=${encodeURIComponent(linkId)}`;
-            console.log(shareUrl);
-            navigator.clipboard.writeText(shareUrl);
-            showShareResponse('link copied to clipboard', 'ok');
-         } else {
-            showShareResponse('Error creating link', 'error');
-         }
-      } catch (error) {
-         showShareResponse('Error creating link', 'error');
-      }
-   });
-
 });
 
-function sourceState() {
-
-   let state =  window.shareLinkState;  // coming from Nunjucks/layout.njk
-   console.log(`loading state:${state}`);
-      if (state) {
-      // get csv values
-      const csvValues = decompressFromBase64(state).split(',');
-      console.log(`loading state as:${csvValues[0]}`);
-
-      // Get the first value which is either "+" or "-"
-      const operation = csvValues[0];
-
-      // Get the list of checkbox IDs
-      const checkboxIds = csvValues.slice(1);
-
-      // Select all checkboxes
-      const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-      // Determine the initial state we want to operate on
-      const checkedValue = (operation === "+") ? false : true;
-
-      // Init with that state
-      checkboxes.forEach(checkbox => checkbox.checked = checkedValue);
-
-      // Toggle the state for the checkboxes whose IDs are in the list
-      checkboxIds.forEach(id => {
-         const checkbox = document.getElementById(id);
-         if (checkbox) checkbox.checked = !checkedValue;
-      });
-   }
-}
-
-function generateState() {
-   // Select only the checkboxes with the specified classes
-   const nameCheckboxes = document.querySelectorAll('.name-checkbox');
-   const versionCheckboxes = document.querySelectorAll('.version-checkbox');
-
-   // Combine into 1 array
-   const allCheckboxes = [...nameCheckboxes, ...versionCheckboxes];
-
-   let checkedIds = ["+"];
-   let uncheckedIds = ["-"];
-
-   allCheckboxes.forEach((checkbox) => {
-         if (checkbox.checked) {
-            checkedIds.push(checkbox.id);
-         } else {
-            uncheckedIds.push(checkbox.id);
-         }
-   });
-
-   if (checkedIds.length > uncheckedIds.length) {
-      return compressToBase64(uncheckedIds.join(','));
-   }
-   return compressToBase64(checkedIds.join(','));
-}
-
-function compressToBase64(inputString) {
-   console.log(inputString);
-   // 1.3 compress into (Uint8Array)
-   const compressedData = pako.deflate(inputString);
-
-   // 2.3 Convert the compressed data (Uint8Array) to a binary string
-   let binaryString = '';
-   for (let i = 0; i < compressedData.length; i++) {
-       binaryString += String.fromCharCode(compressedData[i]);
-   }
-
-   // 3.3 Convert the binary string to a Base64 encoded string
-   const base64String = btoa(binaryString);
-
-   console.log(`produced base64: [${base64String}]`);
-   return base64String;
-}
-
-function decompressFromBase64(base64String) {
-   // 1.3 Base64 to a binary string
-   const binaryString = atob(base64String);
-
-   // 2.3 binary string to a Uint8Array
-   const binaryLength = binaryString.length;
-   const compressedData = new Uint8Array(binaryLength);
-   for (let i = 0; i < binaryLength; i++) {
-       compressedData[i] = binaryString.charCodeAt(i);
-   }
-
-   // 3.3 Decompress Uint8Array to plain text
-   const decompressedData = pako.inflate(compressedData, { to: 'string' });
-
-   return decompressedData;
-}
+//-----------------------------------
+// Add handler to (share/create link)-BUTTON
+//-----------------------------------
+// document.getElementById('button-share').addEventListener('click', async () => {
+//       try {
+//          const base64State = generateState();
+//          const response = await fetch(`/dashboard?savestate=${encodeURIComponent(base64State)}`);
+//          if (response.ok) {
+//             const linkId = await response.text();
+//             const currentUrl = window.location.href;
+//             // Get the base URL without query parameters
+//             const baseUrl = window.location.origin + window.location.pathname;
+//             const shareUrl = `${baseUrl}?linkid=${encodeURIComponent(linkId)}`;
+//             console.log(shareUrl);
+//             navigator.clipboard.writeText(shareUrl);
+//             showShareResponse('link copied to clipboard', 'ok');
+//          } else {
+//             showShareResponse('Error creating link', 'error');
+//          }
+//       } catch (error) {
+//          showShareResponse('Error creating link', 'error');
+//       }
+//    });
 
 
+//-----------------------------------
+// function to show the response's result of share-link button
+//-----------------------------------
 function showShareResponse(message, type) {
    const statusMessage = document.getElementById('share-status-message');
    const statusIcon    = document.getElementById('share-status-icon');
@@ -221,4 +155,102 @@ function showShareResponse(message, type) {
    setTimeout(() => {
        statusMessage.style.display = 'none';
    }, 2000);
+}
+
+
+//-----------------------------------
+// function to init all NAME & VERSION checkboxes from the received 'state'
+//-----------------------------------
+function sourceState() {
+
+   let state =  window.shareLinkState;  // coming from Nunjucks/layout.njk
+   console.log(`loading state:${state}`);
+      if (state) {
+      // get csv values
+      const csvValues = decompressFromBase64(state).split(',');
+      console.log(`loading state as:${csvValues[0]}`);
+
+      // Get the first value which is either "+" or "-"
+      const operation = csvValues[0];
+
+      const checkboxIds = csvValues.slice(1);
+
+      const allCheckboxes = getAllCheckboxes();
+      // Determine the initial state we want to operate on
+      const checkedValue = (operation === "+") ? false : true;
+
+      // Init with that state
+      allCheckboxes.forEach(checkbox => checkbox.checked = checkedValue);
+
+      // Toggle the state for the checkboxes whose IDs are in the list
+      checkboxIds.forEach(id => {
+         const checkbox = document.getElementById(id);
+         if (checkbox) checkbox.checked = !checkedValue;
+      });
+   }
+}
+
+
+//-----------------------------------
+// function to generate a snapshot/state of all NAME & VERSION checkboxes
+//-----------------------------------
+function generateState() {
+
+   const allCheckboxes = getAllCheckboxes();
+   let checkedIds = ["+"];
+   let uncheckedIds = ["-"];
+
+   allCheckboxes.forEach((checkbox) => {
+         if (checkbox.checked) {
+            checkedIds.push(checkbox.id);
+         } else {
+            uncheckedIds.push(checkbox.id);
+         }
+   });
+
+   if (checkedIds.length > uncheckedIds.length) {
+      return compressToBase64(uncheckedIds.join(','));
+   }
+   return compressToBase64(checkedIds.join(','));
+}
+
+//-----------------------------------
+// function to generate a compressed base64 string from a plain text input
+//-----------------------------------
+function compressToBase64(inputString) {
+   console.log(inputString);
+   // 1.3 compress into (Uint8Array)
+   const compressedData = pako.deflate(inputString);
+
+   // 2.3 Convert the compressed data (Uint8Array) to a binary string
+   let binaryString = '';
+   for (let i = 0; i < compressedData.length; i++) {
+       binaryString += String.fromCharCode(compressedData[i]);
+   }
+
+   // 3.3 Convert the binary string to a Base64 encoded string
+   const base64String = btoa(binaryString);
+
+   console.log(`produced base64: [${base64String}]`);
+   return base64String;
+}
+
+//-----------------------------------
+// function to reverse "compressToBase64"
+//-----------------------------------
+function decompressFromBase64(base64String) {
+   // 1.3 Base64 to a binary string
+   const binaryString = atob(base64String);
+
+   // 2.3 binary string to a Uint8Array
+   const binaryLength = binaryString.length;
+   const compressedData = new Uint8Array(binaryLength);
+   for (let i = 0; i < binaryLength; i++) {
+       compressedData[i] = binaryString.charCodeAt(i);
+   }
+
+   // 3.3 Decompress Uint8Array to plain text
+   const decompressedData = pako.inflate(compressedData, { to: 'string' });
+
+   return decompressedData;
 }
