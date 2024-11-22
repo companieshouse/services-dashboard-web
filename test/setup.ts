9a37236bed8ca7230b8ed4f14a430a6d10c11e5c
stefano basile <sbasile@companieshouse.gov.uk>
2024-11-22 16:21:16 +0000

    add init test vars

--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,12 @@
 export default () => {
-  process.env.NODE_ENV = "development";
-  process.env.CDN_HOST = "CDN_HOST";
-
-  //  feature flags
-  process.env.UNESCAPE = "false";
+  
+  process.env.MONGO_PROTOCOL="mongodb+srv"
+  process.env.MONGO_USER="rand_dev"
+  process.env.MONGO_PASSWORD="somepassword"
+  process.env.MONGO_HOST_AND_PORT="ci-dev-pl-0.ueium.mongodb.net"
+  process.env.MONGO_DB_NAME="services_dashboard"
+  process.env.CDN_URL="d3miau0r8stw5u.cloudfront.net"
+  process.env.PORT="3000"
+  process.env.DEP_TRACK_SERVER="https://dependency-track.companieshouse.gov.uk"
+  process.env.SONAR_SERVER="https://code-analysis.platform.aws.chdev.org"
 };
