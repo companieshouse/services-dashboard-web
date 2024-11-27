0b56f2eb7e61451b6517f390eec1ef278acd7801
stefano basile <sbasile@companieshouse.gov.uk>
2024-11-27 20:10:50 +0000

    temp add PORT env var

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ export const CDN_URL = getEnvironmentValue("CDN_URL");
 export const DEP_TRACK_URI =  getEnvironmentValue("DEP_TRACK_SERVER");
 export const SONAR_URI     =  getEnvironmentValue("SONAR_SERVER");
 
-export const PORT = getEnvironmentValue("PORT");
+export const PORT = getEnvironmentValue("PORT","3000");
 export const ENDPOINT_DASHBOARD = getEnvironmentValue("ENDPOINT_DASHBOARD", "/dashboard" );
 export const DAYS_RETENTION_STATE_LINKS = getEnvironmentValue("DAYS_RETENTION_STATE_LINKS", "15");
 
