2dd90a06c41287085e54cd0fa3318f36666c7bff
stefano basile <sbasile@companieshouse.gov.uk>
2025-02-07 18:04:11 +0000

    add css/js skeleton for prodowner

---
+++ public/js/tab-prodowner.js
@@ -0,0 +1,13 @@
+function initialiseTabContent() {
+   const tableId = 'tab-table-id';
+}
+
+function generateTabState() {
+   console.log("generateTabState for tab-prodowner");
+   const tabState = {};
+   return generateState(tabState);
+}
+
+function loadTabFromState(tabId, state) {
+   console.log("loadTabFromState for tab-prodowner");
+}
