656b4e2521bcdefda088b7a1c51fec3e2a05b813
stefano basile <sbasile@companieshouse.gov.uk>
2025-03-04 21:07:17 +0000

    avoid dots in SSM parameters

--- test/setup.ts
+++ test/setup.ts
@@ -1,10 +1,10 @@
 export default () => {
 
-  process.env.MONGO.PROTOCOL="mongodb+srv"
-  process.env.MONGO.USER="rand_dev"
-  process.env.MONGO.PASSWORD="somepassword"
-  process.env.MONGO.HOSTANDPORT="ci-dev-pl-0.ueium.mongodb.net"
-  process.env.MONGO.DBNAME="services_dashboard"
+  process.env.MONGO_PROTOCOL="mongodb+srv"
+  process.env.MONGO_USER="rand_dev"
+  process.env.MONGO_PASSWORD="somepassword"
+  process.env.MONGO_HOSTANDPORT="ci-dev-pl-0.ueium.mongodb.net"
+  process.env.MONGO_DBNAME="services_dashboard"
   process.env.CDN_URL="d3miau0r8stw5u.cloudfront.net"
   process.env.PORT="3000"
   process.env.DEP_TRACK_SERVER="https://dependency-track.companieshouse.gov.uk"
