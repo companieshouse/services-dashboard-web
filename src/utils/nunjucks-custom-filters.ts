5ba71dbd1f115c7c21390c17689b7de7d99dad89
AMoretonCH <amoreton@companieshouse.gov.uk>
2026-07-22 10:20:29 +0100

    Use lastBomImport for date; Use date-fns to format date distance

--- src/utils/nunjucks-custom-filters.ts
+++ src/utils/nunjucks-custom-filters.ts
@@ -1,52 +1,16 @@
 import nunjucksDate from "nunjucks-date-filter";
+import { formatDistanceToNow } from "date-fns";
 
 export const date = nunjucksDate;
 
 export const daysAgo = (date: string | Date): string => {
-    const currentDate = new Date();
-    const targetDate = new Date(date);
-
-    // Check if targetDate is valid.
-      // This is useful when "date" is passed as a string
-      // which may not be converted to a valid date
-      // e.g. a != YYYY-MM-DD (ISO 8601) string
-      // or a generic string: "aaaaa" / "false" / ....
-      // - Formats != ISO 8601 (e.g. dd/mm/yyyy) may still be handled but are not guaranteed
-    if (isNaN(targetDate.getTime())) {
-      return "";   // ignore any unhandled strings
-    }
-    // Calculate time diff (milliseconds)
-    const diffInTime = currentDate.getTime() - targetDate.getTime();
-
-    // Convert to days
-    const diffInDays = Math.floor(diffInTime / (1000 * 60 * 60 * 24));
-
-    if (diffInDays === 0) {
-      return "today";
-    } else if (diffInDays === 1) {
-      return "yesterday";
-    } else if (diffInDays < 0) {
-      return `${Math.abs(diffInDays)} days from now`;
-    } else {
-      return `${diffInDays} days ago`;
+    try {
+      return formatDistanceToNow(new Date(date), { addSuffix: true });
+    } catch (error) {
+      console.error(`Error in daysAgo filter: ${error} for date: ${date}`);
+      return '';
     }
 };
-
-export const daysPassed = (date: string | Date): number => {
-  const currentDate = new Date();
-  const targetDate = new Date(date);
-
-  // Check if targetDate is valid.
-  if (isNaN(targetDate.getTime())) {
-    return NaN;   // return NaN for any unhandled strings
-  }
-  // Calculate time diff (milliseconds)
-  const diffInTime = currentDate.getTime() - targetDate.getTime();
-
-  // Convert to days
-  return Math.floor(diffInTime / (1000 * 60 * 60 * 24));
-};
-
 
 export const urlEncode = (arg: string): string => {
   return encodeURIComponent(arg);
